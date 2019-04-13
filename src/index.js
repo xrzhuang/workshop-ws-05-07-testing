2f1bc5874499aba3aac39f122a25de25333d5acb
Robert He <robert.he.19@dartmouth.edu>
2019-04-12 22:11:42 -0400

    finished starterpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('starting up!');
+// change require to es6 import style
+import $ from 'jquery';
+import './style.scss';
+
+$('#main').html('Here we go!');
+
+let num = 0;
+
+const increment = () => {
+  num += 1;
+  $('#main').html(`You have been on this page for ${num} seconds.`);
+};
+
+setInterval(increment, 1000);
