9739244343fb7bfecfff6923b5ef24fa057aefc4
Robert He <robert.he.19@dartmouth.edu>
2019-04-13 23:57:35 -0400

    final version

--- src/index.js
+++ src/index.js
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './style.scss';
+import debounce from 'lodash.debounce';
+import SearchBar from './components/search_bar';
+import youtubeSearch from './youtube-api';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-const App = () => <div className="test">All the REACT are belong to us!</div>;
+
+// all imports go at the top
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
+    this.search = debounce(this.search, 300);
+    this.search('pixar');
+  }
+
+    search = (text) => {
+      youtubeSearch(text).then((videos) => {
+        this.setState({
+          videos,
+          selectedVideo: videos[0],
+        });
+      });
+    };
+
+    render() {
+      return (
+        <div>
+          <SearchBar id="search-bar" onSearchChange={this.search} />
+          <div id="video-section">
+            <VideoDetail video={this.state.selectedVideo} />
+          </div>
+          <div id="video-section">
+            <VideoList onVideoSelect={selectedVideo => this.setState({ selectedVideo })} videos={this.state.videos} />
+          </div>
+        </div>
+      );
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('main'));
